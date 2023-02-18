ee3f07d36654ad0f1bf1ffbc2e4f38833c14f906
Евгений <volga52@bk.ru>
2023-02-18 22:39:15 +0300

    Add two models Users, Post

---
+++ game_blog/post/models.py
@@ -0,0 +1,22 @@
+"""Создание модели для сущности post"""
+import datetime
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils import UUIDType
+
+from game_blog.db_core import Base
+
+
+class Post(Base):
+    """Модель для БД Pjst"""
+    __tablename__ = "post"
+
+    uid = Column(UUIDType, primary_key=True)
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
+    is_active = Column(Boolean, default=True)
+    title = Column(String)
+    content = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.uid"))
+
+    owner = relationship("User", back_populates="post")
