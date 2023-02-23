28c9d261ffbd78752c5ede0a3b6b35b117f7843d
Евгений <volga52@bk.ru>
2023-02-23 18:52:02 +0300

    Add data schema

---
+++ game_blog/apps/authapp/schemas.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
