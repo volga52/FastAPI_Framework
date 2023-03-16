728766a518ea7a359c891adbe23259ba0179e1e8
Евгений <volga52@bk.ru>
2023-03-16 10:03:38 +0300

    Added elements that create a token

--- game_blog/apps/authapp/models.py
+++ game_blog/apps/authapp/models.py
@@ -1,12 +1,16 @@
 """Модуль создания сущности зарегистрированного пользователя"""
 import datetime
-from uuid import uuid4
+import json
+import time
 
+from authlib.jose import JsonWebSignature
 from sqlalchemy import Boolean, Column, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import EmailType, UUIDType
+from uuid import uuid4
 
 from apps.postsapp.models import Post
+from setting.config import SECRET_KEY
 from database.base_class import Base
 
 
@@ -24,6 +28,31 @@ class User(Base):
 
     post = relationship("Post", back_populates="owner")
     token = Column(String(64), nullable=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        jws = JsonWebSignature(['HS256'])
+        protected = {'alg': 'HS256'}
+        payload = json.dumps({'expires_sec': expires_sec,
+                              'time_sending': time.time(),
+                              'user_uid': str(self.uid)}).encode('utf-8')
+        secret = SECRET_KEY
+        return jws.serialize_compact(protected, payload, secret).\
+            decode('utf-8')
+
+    @staticmethod
+    def get_pay_from_reset_token(token):
+        jws = JsonWebSignature(['HS256'])
+        data = jws.deserialize_compact(token, SECRET_KEY)
+        payload_json = json.loads(data['payload'])
+
+        time_left = payload_json['time_sending'] + payload_json['expires_sec']\
+                    - time.time()
+        print(time_left)
+
+        if time_left < 0:
+            return False
+        else:
+            return payload_json
 
 
 class Token(Base):
